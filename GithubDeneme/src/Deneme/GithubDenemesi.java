dc2711d31db7371c4babade20f9dee378dbfe6d0
syara <syara@10.121.3.114>
2022-10-24 09:06:11 +0300

    Commit 2

---
+++ GithubDeneme/src/Deneme/GithubDenemesi.java
@@ -0,0 +1,13 @@
+package Deneme;
+
+public class GithubDenemesi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Github denemesi");
+
+	}
+
+}
